8b85097a767ccb2192df11e887fe4a329ff5452c
Tal Yehezkel <tal.yehezkel1@mail.huji.ac.il>
2024-12-30 16:12:11 +0200

    Little updates
    Still not working

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMpvement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     public PlayerMovementStats MoveStats;
